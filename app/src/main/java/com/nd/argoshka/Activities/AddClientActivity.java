c50d096f6d1444efa859c24481d850ce96ed4c6a
andrake16 <andrake16@gmail.com>
2016-04-08 18:03:36 +0530

    keep

---
+++ app/src/main/java/com/nd/argoshka/Activities/AddClientActivity.java
@@ -0,0 +1,15 @@
+package com.nd.argoshka.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nd.argoshka.R;
+
+public class AddClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_client);
+    }
+}
--- app/src/main/java/com/nd/argoshka/DataHelper/orm.java
+++
@@ -1,7 +0,0 @@
-package com.nd.argoshka.DataHelper;
-
-/**
- * Created by ND on 2/19/2016.
- */
-public class orm {
-}
